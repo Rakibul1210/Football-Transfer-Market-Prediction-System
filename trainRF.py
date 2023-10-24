fc0ab3891e0da0f9f6507d6b1d72179c20882461
Md. Rakibul Islam <bsse1210@iit.du.ac.bd>
2023-10-25 01:08:55 +0600

    Random forest model trained

---
+++ trainRF.py
@@ -0,0 +1,52 @@
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+from sklearn.model_selection import train_test_split 
+from sklearn.metrics import r2_score
+import pandas as pd
+import joblib
+
+
+
+
+data_path = 'data\data_22-23.csv'
+df = pd.read_csv(data_path)
+
+
+
+# Droping non-numeric colummns for now .........................................................
+df = df.drop(['Name', 'Nation', 'Pos', 'Squad', 'Comp', 'Transfer_fee','Rk'], axis=1)
+
+print(df.dtypes)
+
+
+# Spliting the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(df.drop(columns=['Market_value']), df['Market_value'], test_size=0.2, random_state=42)
+
+# print(X_train.dtypes)
+# print(y_train.dtypes)
+# print(X_test.dtypes)
+# print(y_test.dtypes)
+
+
+
+# Training 
+model = RandomForestRegressor(n_estimators=100, random_state=42) 
+model.fit(X_train, y_train)
+
+# Predictions on the testing set
+y_pred = model.predict(X_test)
+
+# Calculating error metrics
+mae = mean_absolute_error(y_test, y_pred)
+mse = mean_squared_error(y_test, y_pred)
+rmse = mse**0.5
+
+print('Mean Absolute error: ', mae)
+print('Mean squared error: ', mse) 
+print('Root mean squared error: ', rmse)
+
+
+# Save the trained model to a file
+joblib.dump(model, 'models/trained_rf_model.joblib')
+print("Model Saved")
+
